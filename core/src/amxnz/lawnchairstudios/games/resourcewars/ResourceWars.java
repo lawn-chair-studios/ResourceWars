5d7ca91ca2a02ce5e06fa2e4c589051678fa8d2a
Zeale <zeale@dusttoash.org>
2018-09-07 13:21:21 -0500

    Updated Game code to show things. :P

--- core/src/amxnz/lawnchairstudios/games/resourcewars/ResourceWars.java
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/ResourceWars.java
@@ -4,35 +4,43 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class ResourceWars extends ApplicationAdapter {
-	SpriteBatch batch;
-	TiledMap map;
 	OrthographicCamera camera;
 	OrthogonalTiledMapRenderer renderer;
 
+	TiledMap map;
+
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		map = new TmxMapLoader().load("/amxnz/lawnchairstudios/games/resourcewars/assets/levels/levels/Start/map.tmx");
+
 		camera = new OrthographicCamera();
+
+		new FitViewport(20, 20, camera).apply(true);
+
+		map = new TmxMapLoader().load("amxnz/lawnchairstudios/games/resourcewars/assets/levels/levels/Start/map.tmx");
 		renderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
+
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.end();
+
+		camera.update();
+		renderer.setView(camera);
+		renderer.render();
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
+		map.dispose();
+		renderer.dispose();
 	}
 }
