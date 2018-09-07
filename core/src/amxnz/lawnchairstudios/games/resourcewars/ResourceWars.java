342327acce871da09f33b971780cb14eb89e38ee
Zeale <zeale@dusttoash.org>
2018-09-07 14:00:07 -0500

    Organized everything into a Game class

--- core/src/amxnz/lawnchairstudios/games/resourcewars/ResourceWars.java
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/ResourceWars.java
@@ -3,44 +3,27 @@ package amxnz.lawnchairstudios.games.resourcewars;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+
+import amxnz.lawnchairstudios.games.resourcewars.api.Game;
 
 public class ResourceWars extends ApplicationAdapter {
-	OrthographicCamera camera;
-	OrthogonalTiledMapRenderer renderer;
 
-	TiledMap map;
+	private Game game;
 
 	@Override
 	public void create() {
-
-		camera = new OrthographicCamera();
-
-		new FitViewport(20, 20, camera).apply(true);
-
-		map = new TmxMapLoader().load("amxnz/lawnchairstudios/games/resourcewars/assets/levels/levels/Start/map.tmx");
-		renderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
-
+		game = new Game(false);
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		camera.update();
-		renderer.setView(camera);
-		renderer.render();
+		game.render();
 	}
 
 	@Override
 	public void dispose() {
-		map.dispose();
-		renderer.dispose();
+		game.dispose();
 	}
 }
---
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/api/Game.java
@@ -0,0 +1,79 @@
+package amxnz.lawnchairstudios.games.resourcewars.api;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Game {
+
+	private final OrthographicCamera camera = new OrthographicCamera();
+	private Viewport viewport;
+
+	/**
+	 * <p>
+	 * Sets this {@link Game}'s {@link Game#viewport viewport} to be the specified
+	 * viewport, and then applies the viewport.
+	 * </p>
+	 * <p>
+	 * 
+	 * <pre>
+	 * <code>(this.viewport = newViewport).apply(true);</code>
+	 * </pre>
+	 * </p>
+	 * 
+	 * @param newViewport The new {@link Viewport}
+	 */
+	public void setViewport(Viewport newViewport) {
+		(viewport = newViewport).apply(true);
+	}
+
+	/**
+	 * <p>
+	 * <ol>
+	 * <li>Checks if the underyling {@link Viewport}, ({@link #viewport}), is a
+	 * {@link FitViewport}.</li>
+	 * <li>If it is, this method then sets that FitViewport's world size, (using
+	 * {@link FitViewport#setWorldSize(float, float)}), to the specified
+	 * values.</li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * @param width  The new viewport's world width (in game units).
+	 * @param height The new viewport's world height (also in game units).
+	 */
+	public void setViewportWorldSize(float width, float height) {
+		if (viewport instanceof FitViewport)
+			viewport.setWorldSize(width, height);
+	}
+
+	public Viewport getViewport() {
+		return viewport;
+	}
+
+	private final OrthogonalTiledMapRenderer renderer;
+	private final TiledMap map;
+
+	{
+		map = new TmxMapLoader().load("amxnz/lawnchairstudios/games/resourcewars/assets/levels/levels/Start/map.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
+	}
+
+	public Game(boolean stretch) {
+		viewport = stretch ? new FitViewport(20, 20, camera) : new ScreenViewport(camera);
+		viewport.apply(true);
+	}
+
+	public void render() {
+		camera.update();
+		renderer.setView(camera);
+		renderer.render();
+	}
+
+	public void dispose() {
+		renderer.dispose();
+	}
+}
