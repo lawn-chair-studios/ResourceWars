dabfd4099b05dbb750a62dfeb2a869ee21ba7e13
Zeale <zeale@dusttoash.org>
2018-09-10 14:49:32 -0500

    Updated AbstractEntity (added camera binding)

--- core/src/amxnz/lawnchairstudios/games/resourcewars/api/gameplay/entities/AbstractEntity.java
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/api/gameplay/entities/AbstractEntity.java
@@ -1,9 +1,12 @@
 package amxnz.lawnchairstudios.games.resourcewars.api.gameplay.entities;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
@@ -82,6 +85,56 @@ public abstract class AbstractEntity {
 				elapsedAnimationTime = 0;
 			}
 		});
+	}
+
+	private final Map<Camera, Object> cameraBindingMap = new HashMap<Camera, Object>();
+
+	public void cameraBind(final Camera camera, boolean bound) {
+
+		class Binding {
+
+			private final Observer<Float> x, y;
+
+			private Binding(final Camera camera) {
+				x = new Observer<Float>() {
+
+					@Override
+					public void espy(Float oldValue, Float newValue) {
+						if ((float) oldValue != newValue) {
+							camera.position.x = newValue;
+							camera.update();
+						}
+					}
+				};
+				y = new Observer<Float>() {
+
+					@Override
+					public void espy(Float oldValue, Float newValue) {
+						if ((float) oldValue != newValue) {
+							camera.position.y = newValue;
+							camera.update();
+						}
+					}
+				};
+				AbstractEntity.this.x.addObserver(x);
+				AbstractEntity.this.y.addObserver(y);
+				cameraBindingMap.put(camera, this);
+			}
+
+			public void unbind() {
+				AbstractEntity.this.x.removeObserver(x);
+				AbstractEntity.this.y.removeObserver(y);
+				cameraBindingMap.remove(camera);
+			}
+
+		}
+
+		if (!bound) {
+			if (cameraBindingMap.containsKey(camera))
+				((Binding) cameraBindingMap.get(camera)).unbind();
+		} else if (!cameraBindingMap.containsKey(camera))
+			new Binding(camera);
+
 	}
 
 	public float getX() {
