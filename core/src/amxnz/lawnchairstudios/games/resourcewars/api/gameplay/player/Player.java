c84239affa05ff0f02b27ef29658cb86e53d5d42
Zeale <zeale@dusttoash.org>
2018-09-10 12:52:36 -0500

    Updated Player class

--- core/src/amxnz/lawnchairstudios/games/resourcewars/api/gameplay/player/Player.java
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/api/gameplay/player/Player.java
@@ -1,60 +1,20 @@
 package amxnz.lawnchairstudios.games.resourcewars.api.gameplay.player;
 
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-
 import amxnz.lawnchairstudios.games.resourcewars.api.gameplay.entities.AbstractEntity;
 
+public class Player {
 
-public class Player implements InputProcessor {
+	public Player(AbstractEntity inGameCharacter) {
+		this.inGameCharacter = inGameCharacter;
+	}
+
+	public void setInGameCharacter(AbstractEntity inGameCharacter) {
+		this.inGameCharacter = inGameCharacter;
+	}
+
 	private AbstractEntity inGameCharacter;
 
-	@Override
-	public boolean keyDown(int keycode) {
-		if(keycode==Input.Keys.A);
-		return false;
+	public AbstractEntity getInGameCharacter() {
+		return inGameCharacter;
 	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
