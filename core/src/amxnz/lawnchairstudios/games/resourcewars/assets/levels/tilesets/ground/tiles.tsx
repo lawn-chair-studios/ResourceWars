9ce7433a95d81ce68bd097b412cb4f7fafd5b297
Zeale <zeale@dusttoash.org>
2018-09-10 22:53:53 -0500

    Updated test map

--- core/src/amxnz/lawnchairstudios/games/resourcewars/assets/levels/tilesets/ground/tiles.tsx
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/assets/levels/tilesets/ground/tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="Grass-1.png"/>
@@ -18,5 +18,17 @@
  </tile>
  <tile id="5">
   <image width="32" height="32" source="Grass-3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="29" height="24" source="GrassOverlay-1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="GrassOverlay-2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="GrassOverlay-3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="GrassOverlay-4.png"/>
  </tile>
 </tileset>
