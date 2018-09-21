7c36ea6d935fb9959842cc140894c6b50cff063e
Zeale <zeale@dusttoash.org>
2018-09-21 17:13:10 -0500

    Updated things

--- core/src/amxnz/lawnchairstudios/games/resourcewars/assets/levels/tilesets/ground/tiles.tsx
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/assets/levels/tilesets/ground/tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="10" columns="0">
+<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="36" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="Grass-1.png"/>
@@ -30,5 +30,83 @@
  </tile>
  <tile id="10">
   <image width="32" height="32" source="GrassOverlay-4.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="PureWater-1.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="cliffs/cliff-grass-1.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="cliffs/cliff-grass-2.png"/>
+ </tile>
+ <tile id="44">
+  <image width="32" height="32" source="cliffs/cliff-grass-3.png"/>
+ </tile>
+ <tile id="45">
+  <image width="32" height="32" source="cliffs/cliff-grass-4.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="cliffs/cliff-grass-5.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="cliffs/cliff-grass-6.png"/>
+ </tile>
+ <tile id="48">
+  <image width="32" height="32" source="cliffs/cliff-grass-7.png"/>
+ </tile>
+ <tile id="49">
+  <image width="32" height="32" source="cliffs/cliff-grass-8.png"/>
+ </tile>
+ <tile id="50">
+  <image width="32" height="32" source="cliffs/cliff-grass-9.png"/>
+ </tile>
+ <tile id="51">
+  <image width="32" height="32" source="cliffs/cliff-grass-10.png"/>
+ </tile>
+ <tile id="52">
+  <image width="32" height="32" source="cliffs/cliff-grass-11.png"/>
+ </tile>
+ <tile id="53">
+  <image width="32" height="32" source="cliffs/cliff-grass-12.png"/>
+ </tile>
+ <tile id="54">
+  <image width="32" height="32" source="cliffs/cliff-ocean-1.png"/>
+ </tile>
+ <tile id="55">
+  <image width="32" height="32" source="cliffs/cliff-ocean-2.png"/>
+ </tile>
+ <tile id="56">
+  <image width="32" height="32" source="cliffs/cliff-ocean-3.png"/>
+ </tile>
+ <tile id="57">
+  <image width="32" height="32" source="cliffs/cliff-ocean-4.png"/>
+ </tile>
+ <tile id="58">
+  <image width="32" height="32" source="cliffs/cliff-ocean-5.png"/>
+ </tile>
+ <tile id="59">
+  <image width="32" height="32" source="cliffs/down-cliff-side-1.png"/>
+ </tile>
+ <tile id="60">
+  <image width="32" height="32" source="cliffs/down-cliff-side-2.png"/>
+ </tile>
+ <tile id="61">
+  <image width="32" height="32" source="cliffs/down-cliff-side-3.png"/>
+ </tile>
+ <tile id="62">
+  <image width="32" height="32" source="cliffs/down-cliff-side-4.png"/>
+ </tile>
+ <tile id="63">
+  <image width="32" height="32" source="cliffs/down-cliff-side-5.png"/>
+ </tile>
+ <tile id="64">
+  <image width="32" height="32" source="cliffs/down-cliff-side-6.png"/>
+ </tile>
+ <tile id="65">
+  <image width="32" height="32" source="cliffs/down-cliff-side-7.png"/>
+ </tile>
+ <tile id="66">
+  <image width="32" height="32" source="cliffs/down-cliff-side-8.png"/>
  </tile>
 </tileset>
