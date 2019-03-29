3c67b411df7d82343863eb7ad4008d6062445114
Zeale <zeale@dusttoash.org>
2019-03-29 12:36:52 -0500

    Updated some resources

---
+++ core/src/amxnz/lawnchairstudios/games/resourcewars/assets/levels/tilesets/ground/buildings-1/tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Buildings-1" tilewidth="640" tileheight="288" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="96" source="Shack-1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="640" height="288" source="Building-1.png"/>
+ </tile>
+</tileset>
